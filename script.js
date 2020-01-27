917400d785fc71df7ffa43cff23135efbc9c1c3b
Nick Wang <nickyiliwang@gmail.com>
2020-01-27 14:07:16 -0500

    current

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const contentContainer = document.querySelector(".contentContainer");
 
 fetch(
-  "https://old.reddit.com/r/AnimalsBeingJerks/top.json?sort=top&t=${timeQuery}&limit=30"
+  "https://old.reddit.com/r/AnimalsBeingJerks/top.json?sort=top&t=${timeQuery}&limit=50"
 )
   .then(response => response.json())
   .then(json => {
-    console.log(json.data.children);
     json.data.children.forEach(post => {
       const { id, url, is_video } = post.data;
-      console.log(id);
-      console.log(url);
-      console.log(is_video);
-
       if (!is_video) {
         const img = document.createElement("img");
         img.src = url;
         contentContainer.appendChild(img);
+      } else if (is_video) {
+        const videoUrl = post.data.media.reddit_video.fallback_url;
+        const video = document.createElement("video");
+        video.src = videoUrl;
+        video.autoplay = true;
+        video.muted = true;
+        video.nodeType = "video/mp4";
+        contentContainer.appendChild(video);
       }
     });
   });
